20666f4fa1272c0412d0fe2d8351c0675d744007
Milena <milenasvitras@gmail.com>
2025-11-07 20:09:13 -0300

    :sparkles: Atualização dos arquivos style.css, sucess.html, index.html e scripts.js

--- exemplos_fundweb/06_js_eventos/assets/js/scripts.js
+++ exemplos_fundweb/06_js_eventos/assets/js/scripts.js
@@ -1,1 +1,20 @@
-﻿
+﻿// Selecionando elementos HTML
+const input_change = document.querySelector("#input_change");
+const uppercase = document.querySelector("#uppercase");
+const botao_clique = document.querySelector("#botao_clique");
+const botao_over = document.querySelector("#botao_over");
+
+// Função para converter texto para maiúsculas
+function upperCase(){
+    uppercase.innerHTML = input_change.value.toUpperCase();
+}
+
+// Adicionando evento click ao elemento
+botao_clique.addEventListener("click", () =>{
+    alert("O botão foi clicado!");
+})
+
+// Adicionando evento Mouse Over ao elemento
+botao_over.addEventListener("mouseover", () =>{
+    alert("O mouse passou sobre o botão!");
+})
